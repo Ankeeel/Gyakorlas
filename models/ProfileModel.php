22932203208632fdf42fb2dde7001d58ba22555d
Ankeeel <bemy4ngel@gmail.com>
2016-11-09 20:38:15 +0100

    nesze

--- models/ProfileModel.php
+++ models/ProfileModel.php
@@ -2,10 +2,21 @@
 
 class ProfileModel extends Model
 {
+  public $ertek;
     public function personalData($id){
         $stmt = $this->db->prepare("SELECT * FROM users WHERE id=:id");
         $stmt->execute(array('id'=>$id));
         return $stmt->fetch();
+    }
+
+    public function like(){
+    $this->ertek++;
+        header('Location: /login');
+}
+
+    public function dislike(){
+        $this->ertek--;
+        header('Location: /profile/edit');
     }
 
 }
--- models/SettingsModel.php
+++ models/SettingsModel.php
@@ -4,18 +4,30 @@
 class SettingsModel extends Model
 {
     public function settingsSave (){
-        $stmt = $this->db->prepare("SELECT * FROM users WHERE id=:id");
+        $stmt = $this->db->prepare("SELECT * FROM persetting WHERE userId=:id");
         $stmt->execute(array('id'=>Session::get('user')));
-
-        $stmt = $this->db->prepare("INSERT INTO persetting (city,school,job,relationship) VALUES (:city,:school,:job,:relationship)");
-        $stmt->execute(array(
-        'city' => $_POST['city'],
-        'school' => $_POST['school'],
-        'job' => $_POST['job'],
-        'relationship' => $_POST['relationship'],
-        'id' => Session::get('user')
-        ));
+        if($stmt->rowCount()>0){
+            $stmt = $this->db->prepare("UPDATE persetting SET city=:city,school=:school,job=:job,relationship=:relationship WHERE userId=:userId ");
+            $stmt->execute(array(
+                'city' => $_POST['city'],
+                'school' => $_POST['school'],
+                'job' => $_POST['job'],
+                'relationship' => $_POST['relationship'],
+                'userId' => Session::get('user')
+            ));
+        }
+            else{
+                $stmt = $this->db->prepare("INSERT INTO persetting (city,school,job,relationship,userId) VALUES (:city,:school,:job,:relationship,:userId)");
+                $stmt->execute(array(
+                    'city' => $_POST['city'],
+                    'school' => $_POST['school'],
+                    'job' => $_POST['job'],
+                    'relationship' => $_POST['relationship'],
+                    'userId' => Session::get('user')
+                ));
+            }
         header('Location: /settings/personalsetting');
+
     }
 
     public function personalData(){
